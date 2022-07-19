0dddb850aea6ff79110094f408124e88513bc9e8
Nels <nelspd915@gmail.com>
2022-07-19 16:31:56 -0600

    Updates

---
+++ build/codenames.esm.js
@@ -0,0 +1,2 @@
+import{p as e,b as o}from"./p-4578d37f.js";const a=()=>{const o=import.meta.url;const a={};if(o!==""){a.resourcesUrl=new URL(".",o).href}return e(a)};a().then((e=>o([["p-f034abe4",[[1,"codenames-board",{cellData:[16]}],[1,"codenames-cell",{word:[1],color:[1],mode:[1],revealed:[4]}]]]],e)));
+//# sourceMappingURL=codenames.esm.js.map
