0799d7a43e9028c20faf1de2c8d263a0872bfa3a
Nels <nelspd915@gmail.com>
2022-07-19 20:36:55 -0600

    test: remove e2e tests

--- src/components/codenames-app/codenames-app.spec.tsx
+++ src/components/codenames-app/codenames-app.spec.tsx
@@ -1,5 +1,5 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { CodenamesApp } from '../codenames-app';
+import { CodenamesApp } from './codenames-app';
 
 describe('codenames-app', () => {
   it('renders', async () => {
