0c9cea32a8b12350103b5ea0caa69cbec228ae2c
Nels <nelspd915@gmail.com>
2022-07-19 20:03:07 -0600

    Updates

--- app/main.js
+++
@@ -1,6 +0,0 @@
-import * as axios from "axios"
-
-const codeboard = document.getElementById("codeboard");
-axios.get('http://127.0.0.1:8000/api/dummyData').then((res) =>{
-  codeboard.cellData = res.data;
-});
