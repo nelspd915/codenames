b0707fff7db5daa4803fb69016019eab940c3565
Saguuu <hjaros1997@gmail.com>
2022-07-21 01:39:40 -0500

    feat: add socket.io to backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,19 @@ const app = express();
 const cors = require("cors");
 const PORT = 8080;
 
+// Initialize web sockets with socket.io
+const io = require("socket.io")(8000, {
+    cors: {
+        origin: ["http://localhost:3333"]
+    }
+});
+
+io.on("connection", socket => {
+    socket.on("revealCell", (cell) => {
+        console.log(cell);
+    });
+});
+
 // Initialize middleware for server
 app.use(express.json());
 app.use(cors());
@@ -14,7 +27,7 @@ const gameBoard = require("./gameboard");
 // Api routes
 app.get("/gameboard", (req, res) => {
     res.status(200).send(gameBoard);
-})
+});
 
 // Open server to listen for requests
 app.listen(
--- src/components/codenames-spinner/readme.md
+++ src/components/codenames-spinner/readme.md
@@ -5,6 +5,19 @@
 <!-- Auto Generated Below -->
 
 
+## Dependencies
+
+### Used by
+
+ - [codenames-cell](../codenames-cell)
+
+### Graph
+```mermaid
+graph TD;
+  codenames-cell --> codenames-spinner
+  style codenames-spinner fill:#f9f,stroke:#333,stroke-width:4px
+```
+
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
