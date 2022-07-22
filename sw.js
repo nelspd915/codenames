19dee2b6774677fde08507dd694b6c5b8b3430ff
Nels <nelspd915@gmail.com>
2022-07-21 20:49:24 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,14 +27,35 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "688a4d5415c374944af10a42ba6955c5"
+    "revision": "6388e0b28ca8ff4735db286f36829c9d"
   },
   {
     "url": "build/index.esm.js",
     "revision": "559a2ea214d9c794dd7b520bc1900f56"
   },
   {
-    "url": "build/p-067278d3.entry.js"
+    "url": "build/p-0ccad4bc.js"
+  },
+  {
+    "url": "build/p-4d0c4345.entry.js"
+  },
+  {
+    "url": "build/p-4f5c3fbf.entry.js"
+  },
+  {
+    "url": "build/p-5401066b.js"
+  },
+  {
+    "url": "build/p-5854c8f5.entry.js"
+  },
+  {
+    "url": "build/p-6899f280.entry.js"
+  },
+  {
+    "url": "build/p-6a1e8d93.entry.js"
+  },
+  {
+    "url": "build/p-6c0b4435.js"
   },
   {
     "url": "build/p-78ab5146.js"
@@ -43,8 +64,29 @@ self.__precacheManifest = [
     "url": "build/p-9f42e523.js"
   },
   {
+    "url": "build/p-a46eb8a3.entry.js"
+  },
+  {
+    "url": "build/p-b09aada7.entry.js"
+  },
+  {
+    "url": "build/p-b4ade62a.js"
+  },
+  {
+    "url": "build/p-c3215bb5.entry.js"
+  },
+  {
+    "url": "build/p-df024099.entry.js"
+  },
+  {
+    "url": "build/p-e8605e2f.js"
+  },
+  {
+    "url": "build/p-f68e23d0.js"
+  },
+  {
     "url": "main.js",
-    "revision": "ea4ec1a055adeff3fd5d40b7f944cebc"
+    "revision": "80fd369622bdd5781e933e3b48192ddc"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
