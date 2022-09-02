d8f362c1e631e69354bd9a6dbf2fb1ae2460c871
Nels <nelspd915@gmail.com>
2022-09-01 20:09:23 -0500

    test: fix test cases by adding a mock server

--- packages/frontend/src/components/codenames-board/codenames-board.spec.tsx
+++ packages/frontend/src/components/codenames-board/codenames-board.spec.tsx
@@ -1,12 +1,18 @@
 import { h } from "@stencil/core";
 import { newSpecPage } from "@stencil/core/testing";
+import { Server } from "../../extra/types";
 import { CodenamesBoard } from "./codenames-board";
 
 describe("codenames-board", () => {
+  const mockServer = {
+    socket: {
+      on: () => {}
+    }
+  } as unknown as Server;
   it("renders", async () => {
     const page = await newSpecPage({
       components: [CodenamesBoard],
-      template: () => <codenames-board server={{}}></codenames-board>
+      template: () => <codenames-board server={mockServer}></codenames-board>
     });
     expect(page.root).toBeTruthy();
   });
--- packages/frontend/src/components/codenames-landing-page/codenames-landing-page.spec.tsx
+++ packages/frontend/src/components/codenames-landing-page/codenames-landing-page.spec.tsx
@@ -1,12 +1,18 @@
 import { h } from "@stencil/core";
 import { newSpecPage } from "@stencil/core/testing";
+import { Server } from "../../extra/types";
 import { CodenamesLandingPage } from "./codenames-landing-page";
 
 describe("codenames-landing-page", () => {
+  const mockServer = {
+    socket: {
+      on: () => {}
+    }
+  } as unknown as Server;
   it("renders", async () => {
     const page = await newSpecPage({
       components: [CodenamesLandingPage],
-      template: () => <codenames-landing-page server={{}}></codenames-landing-page>
+      template: () => <codenames-landing-page server={mockServer}></codenames-landing-page>
     });
     expect(page.root).toBeTruthy();
   });
