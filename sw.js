ca907cdcd6b1d3774450b03be26ed566acb0f210
Nels <nelspd915@gmail.com>
2022-09-01 23:12:39 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "e79544204e4139641223ad0b9cc4941b"
+    "revision": "31d240ee59df340862c0f89802d8538a"
   },
   {
     "url": "app/main.css",
